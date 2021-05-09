6201fbeb3615ae854fc2866eb45fc8ddc82b6df8
zakaria99884 <zakariaelkashef@gmail.com>
2021-05-09 22:47:19 +0300

    no-03

--- content/js/script.js
+++ content/js/script.js
@@ -1,20 +1,45 @@
 // typeit
 new TypeIt("#type-1", {
-    speed: 160,
+    speed: 120,
     loop: true,
     waitUntilVisible: true,
   })
-    .type("Designer", { delay: 300 })
+    .type("Designer", { delay: 400 })
     .pause(500)
     .delete(9)
-    .type("Developer", { delay: 300 })
+    .type("Developer", { delay: 400 })
+    .pause(500)
+    .delete(9)
+    .go();
+new TypeIt("#type-2", {
+    speed: 120,
+    loop: true,
+    waitUntilVisible: true,
+  })
+    .type("Designer", { delay: 400 })
+    .pause(500)
+    .delete(9)
+    .type("Developer", { delay: 400 })
     .pause(500)
     .delete(9)
     .go();
 
 // gsap
-gsap.from(".nav__logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
-gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 1, y: -50 });
-gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 1.5, y: -45 });
-gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 2, y: -30 });
-gsap.from(".hero a", { opacity: 0, duration: 1, delay: 2.5, y: 50 });
+gsap.from(".nav__logo", { opacity: 0, duration: 1, delay: 0.3, y: -50 });
+gsap.from(".navbar-nav", { opacity: 0, duration: 1, delay: 0.4, y: -50 });
+gsap.from(".header__img", { opacity: 0, duration: 1, delay: 0.5, x: 50 });
+gsap.from(".hero", { opacity: 0, duration: 1, delay: 0.6, y: -50 });
+gsap.from(".nav-item", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 1.2,
+  y: -30,
+  stagger: 0.2,
+});
+gsap.from(".header__icons a", {
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+  x: -30,
+  stagger: 0.2,
+});
